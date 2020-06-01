44a41e94fa815e28a7f80677bbca20fd4a136bb2
David S. García (Azæl) <azael@dragonspark.tech>
2020-05-31 23:32:23 -0500

    chore: register seeders into main class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,11 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UserSeeder::class);
+    public function run() {
+        $this -> call([
+            ApplicationTargetsSeeder::class,
+            ApplicationTypesSeeder::class,
+            EmployeesSeeder::class
+        ]);
     }
 }
